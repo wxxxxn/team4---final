3a3ebc44029a6f72261ff252a7efc6082b52b8bc
GGG <GGG@DESKTOP-8N3GG4O>
2023-01-04 17:07:02 +0900

    활동 기능창 수정(버튼 이미지 추가, 배경 투명 등)

--- team4 - developerGrow/src/gui/ActiveDialog.java
+++ team4 - developerGrow/src/gui/ActiveDialog.java
@@ -1,109 +1,123 @@
 package gui;
 
 import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.SwingConstants;
 
 public class ActiveDialog extends JDialog {
-
-	private JPanel contentPane;
-
+	private JButton[] btn = new JButton[7];
+	private ClassLoader classLoader;
 	public ActiveDialog(int x, int y) {
-		
 		setUndecorated(true);
 		setModal(true);
 		setBounds(x + 7, y + 30, 1185, 762);
-		setBackground(new Color(0, 0, 0, 100));
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setBackground(new Color(0, 0, 0, 0));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("상하차");
-		btnNewButton.setBounds(420, 110, 130, 45);
-		contentPane.add(btnNewButton);
-		btnNewButton.setOpaque(false);
-		btnNewButton.setBorderPainted(false);
+		setBackground(new Color(0, 0, 0, 80));
+		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("활동");	
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("HY목각파임B", Font.BOLD, 40));
-		lblNewLabel.setBounds(12, 10, 1161, 50);
-		contentPane.add(lblNewLabel);
+		classLoader = getClass().getClassLoader();
 		
-		JButton btnNewButton_1 = new JButton("이력서 작성");
-		btnNewButton_1.setBounds(420, 210, 130, 45);
-		contentPane.add(btnNewButton_1);
-		btnNewButton_1.setOpaque(false);
-		btnNewButton_1.setBorderPainted(false);
+		// 상하차(0), 밥먹기, 게임하기, 잠자기, 담배피기, 이력서 작성, 뒤로(6)
+		for (int i = 0; i < 7; i++) {
+			btn[i] = new JButton(imageIcon(i));
+			btn[i].setPressedIcon(imageIconPush(i));
+			add(btn[i]);
+			btn[i].setBorderPainted(false);
+			btn[i].setFocusPainted(false);
+			btn[i].setContentAreaFilled(false);
+			btn[i].addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Object command = e.getSource();
+					for (int i = 0; i < 7; i++) {
+						if (command == btn[i]) {
+							active(i); // 버튼 입력값 -> 작동
+						}
+					}
+					dispose();
+				}
+			});
+		}
+		btn[0].setBounds(470, 200, 100, 50);
+		btn[1].setBounds(630, 200, 100, 50);
+		btn[2].setBounds(400, 300, 100, 50);
+		btn[3].setBounds(700, 300, 100, 50);
+		btn[4].setBounds(400, 400, 100, 50);
+		btn[5].setBounds(700, 400, 100, 50);
+		btn[6].setBounds(500, 550, 200, 50);
 		
-		JButton btnNewButton_2 = new JButton("담배피기");
-		btnNewButton_2.setBounds(420, 310, 130, 45);
-		contentPane.add(btnNewButton_2);
-		btnNewButton_2.setBorderPainted(false);
-		btnNewButton_2.setOpaque(false);
 		
-		JButton btnNewButton_3 = new JButton("밥먹기");
-		btnNewButton_3.setBounds(420, 410, 130, 45);
-		contentPane.add(btnNewButton_3);
-		
-		
-		JButton btnNewButton_4 = new JButton("잠자기");
-		btnNewButton_4.setBounds(420, 510, 130, 45);
-		contentPane.add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("게임하기");
-		btnNewButton_5.setBounds(420, 610, 130, 45);
-		contentPane.add(btnNewButton_5);
-		
-		JLabel lblNewLabel_1 = new JLabel("설명");
-		lblNewLabel_1.setBounds(653, 110, 520, 45);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("New label");
-		lblNewLabel_1_1.setBounds(653, 210, 520, 45);
-		contentPane.add(lblNewLabel_1_1);
-		
-		JLabel lblNewLabel_1_2 = new JLabel("New label");
-		lblNewLabel_1_2.setBounds(653, 310, 520, 45);
-		contentPane.add(lblNewLabel_1_2);
-		
-		JLabel lblNewLabel_1_3 = new JLabel("New label");
-		lblNewLabel_1_3.setBounds(653, 410, 520, 45);
-		contentPane.add(lblNewLabel_1_3);
-		
-		JLabel lblNewLabel_1_4 = new JLabel("New label");
-		lblNewLabel_1_4.setBounds(653, 510, 520, 45);
-		contentPane.add(lblNewLabel_1_4);
-		
-		JLabel lblNewLabel_1_5 = new JLabel("New label");
-		lblNewLabel_1_5.setBounds(653, 610, 520, 45);
-		contentPane.add(lblNewLabel_1_5);
-		
-		JButton btnNewButton_6 = new JButton("닫기");
-		btnNewButton_6.setBounds(535, 700, 130, 45);
-		btnNewButton_6.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				dispose();
-			}
-			
-		});
-		contentPane.add(btnNewButton_6);
+		// +8 +30 -16 -38
+//		setBounds(108, 130, 1184, 762);
 	}
-
+	
 	public void showGUI() {
 		setVisible(true);
+	}
+	
+	// 추가
+	public static void active (int num) {
+		switch (num) {
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			default:
+				break;
+		}
+	}
+	public ImageIcon imageIcon (int num) {
+		switch (num) {
+			case 0:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeCouPang.png"));
+			case 1:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeEat.png"));
+			case 2:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeGame.png"));
+			case 3:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeSleep.png"));
+			case 4:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeSmokeing.png"));
+			case 5:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeWrite.png"));
+			case 6:
+				return new ImageIcon(classLoader.getResource("btn_image/closeBtnImage.png"));
+			default:
+				return null;
+		}
+	}
+	public ImageIcon imageIconPush (int num) {
+		switch (num) {
+			case 0:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeCouPangPush.png"));
+			case 1:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeEatPush.png"));
+			case 2:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeGamePush.png"));
+			case 3:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeSleepPush.png"));
+			case 4:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeSmokeingPush.png"));
+			case 5:
+				return new ImageIcon(classLoader.getResource("activeBtn_image/activeWritePush.png"));
+			case 6:
+				return new ImageIcon(classLoader.getResource("btn_image/closeBtnPushImage.png"));
+			default:
+				return null;
+		}
 	}
 }
