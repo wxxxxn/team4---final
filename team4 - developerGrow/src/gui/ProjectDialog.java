3c7f913009825240c8134a21dd12dbec9807bde6
GGG <GGG@DESKTOP-8N3GG4O>
2023-01-05 11:38:38 +0900

    업무창 수정

--- team4 - developerGrow/src/gui/ProjectDialog.java
+++ team4 - developerGrow/src/gui/ProjectDialog.java
@@ -1,98 +1,114 @@
 package gui;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import custom_panel.ProjectPanel;
 
 public class ProjectDialog extends JDialog {
 
 	private JPanel contentPane;
 
 	public ProjectDialog(int x, int y) {
+		
 		setUndecorated(true);
 		setModal(true);
 		setBounds(x + 7, y + 30, 1185, 762);
 		setBackground(new Color(0, 0, 0, 100));
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setOpaque(false);
 		contentPane.setBackground(new Color(0, 0, 0, 0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+		JLabel rankTextPanel = new JLabel("프로젝트");
+		rankTextPanel.setBackground(Color.WHITE);
+		rankTextPanel.setHorizontalAlignment(SwingConstants.CENTER);
+		rankTextPanel.setFont(new Font("HY목각파임B", Font.BOLD, 40));
+		rankTextPanel.setBounds(12, 10, 1161, 50);
+		contentPane.add(rankTextPanel);
+
+		JPanel ProjectsPanel = new JPanel();
+		ProjectsPanel.setOpaque(false);
+		ProjectsPanel.setBackground(new Color(0, true));
+		ProjectsPanel.setLayout(new BoxLayout(ProjectsPanel, BoxLayout.Y_AXIS));
+
+		ProjectPanel[] pj = new ProjectPanel[17];
+		List<String> projectList = new ArrayList<>();
+		projectList.add("자바 기초 및 프로그래밍 기초");
+		projectList.add("선택, 반복, 배열");
+		projectList.add("객체 지향 소개 및 클래스, 객체, 메소드");
+		projectList.add("클래스와 메소드 심층 연구");
+		projectList.add("상속, 그래픽 사용자 인터페이스");
+		projectList.add("추상 클래스, 인터페이스, 패키지");
+		projectList.add("자바 API 패키지, 예외처리, 모듈");
+		projectList.add("중간 평가");
+		projectList.add("자바 GUI 기초, 이벤트 처리");
+		projectList.add("스윙 컴포넌트, 자바 그래픽");
+		projectList.add("제네릭과 컬렉션");
+		projectList.add("함수형 프로그래밍, 스트림, 람다식");
+		projectList.add("파일 입출력");
+		projectList.add("멀티 스레딩");
+		projectList.add("네트워크 프로그래밍");
+		projectList.add("데이터베이스 프로그래밍");
+		projectList.add("기말 평가");
 		
-		JButton btnNewButton = new JButton("Hello world 출력");
-		btnNewButton.setBounds(420, 110, 130, 45);
-		contentPane.add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("업무(프로젝트)");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("HY목각파임B", Font.BOLD, 40));
-		lblNewLabel.setBounds(12, 10, 1161, 50);
-		contentPane.add(lblNewLabel);
-		
-		JButton btnNewButton_1 = new JButton("이력서 작성");
-		btnNewButton_1.setBounds(420, 210, 130, 45);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("담배피기");
-		btnNewButton_2.setBounds(420, 310, 130, 45);
-		contentPane.add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("밥먹기");
-		btnNewButton_3.setBounds(420, 410, 130, 45);
-		contentPane.add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("잠자기");
-		btnNewButton_4.setBounds(420, 510, 130, 45);
-		contentPane.add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("게임하기");
-		btnNewButton_5.setBounds(420, 610, 130, 45);
-		contentPane.add(btnNewButton_5);
-		
-		JLabel lblNewLabel_1 = new JLabel("설명");
-		lblNewLabel_1.setBounds(653, 110, 520, 45);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("New label");
-		lblNewLabel_1_1.setBounds(653, 210, 520, 45);
-		contentPane.add(lblNewLabel_1_1);
-		
-		JLabel lblNewLabel_1_2 = new JLabel("New label");
-		lblNewLabel_1_2.setBounds(653, 310, 520, 45);
-		contentPane.add(lblNewLabel_1_2);
-		
-		JLabel lblNewLabel_1_3 = new JLabel("New label");
-		lblNewLabel_1_3.setBounds(653, 410, 520, 45);
-		contentPane.add(lblNewLabel_1_3);
-		
-		JLabel lblNewLabel_1_4 = new JLabel("New label");
-		lblNewLabel_1_4.setBounds(653, 510, 520, 45);
-		contentPane.add(lblNewLabel_1_4);
-		
-		JLabel lblNewLabel_1_5 = new JLabel("New label");
-		lblNewLabel_1_5.setBounds(653, 610, 520, 45);
-		contentPane.add(lblNewLabel_1_5);
-		
-		JButton btnNewButton_6 = new JButton("닫기");
-		btnNewButton_6.setBounds(535, 700, 130, 45);
-		btnNewButton_6.addActionListener(new ActionListener() {
+		for (int i = 0; i < pj.length; i++) {
+			ProjectPanel project = new ProjectPanel();
+			project.getRankLabel().setText(String.valueOf((i + 1) + "주차"));
+			project.getProjectName().setText(projectList.get(i));
+			pj[i] = project;
+			ProjectsPanel.add(pj[i]);
+			ProjectsPanel.add(Box.createVerticalStrut(25));
+		}
+
+		JScrollPane scrollPane = new JScrollPane(ProjectsPanel);
+		scrollPane.setOpaque(false);
+		scrollPane.setBackground(new Color(0, true));
+		scrollPane.getViewport().setOpaque(false);
+		scrollPane.setBounds(312, 72, 560, 619);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		contentPane.add(scrollPane);
+
+		ClassLoader classLoader = getClass().getClassLoader();
+
+		URL URLCloseButtonImage = classLoader.getResource("btn_image/closeBtnImage.png");
+		ImageIcon CloseButtonIcon = new ImageIcon(URLCloseButtonImage);
+
+		URL URLCloseButtonPushImage = classLoader.getResource("btn_image/closeBtnPushImage.png");
+		ImageIcon CloseButtonPushIcon = new ImageIcon(URLCloseButtonPushImage);
+
+		JButton closeButton = new JButton();
+		closeButton.setBounds(488, 701, 200, 50);
+		closeButton.setIcon(CloseButtonIcon);
+		closeButton.setPressedIcon(CloseButtonPushIcon);
+		closeButton.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		contentPane.add(btnNewButton_6);
+		contentPane.add(closeButton);
+
 	}
 
 	public void showGUI() {
