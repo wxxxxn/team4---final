a8f82b100a60a8e7be9d7152fc728284b7f739dd
GGG <GGG@DESKTOP-8N3GG4O>
2023-01-09 10:44:11 +0900

    로그인 회원가입 db연동 및 코드 수정

--- team4 - developerGrow/src/login/AccountDelete.java
+++ team4 - developerGrow/src/login/AccountDelete.java
@@ -1,19 +1,21 @@
-package accountData;
+package login;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 import database.util.ConnectionProvider;
 
-public class Login {
-	public static String checkUserId(String userId) { // 아디 입력 받음
-		String sql = "SELECT userPw FROM team4.user where userId = ?";
+public class AccountDelete {
+	
+//	accountDelete(checkUserPw(checkUserId(id), inputPw), id);
+	public static String checkUserId(int id) { // 아디 입력 받음
+		String sql = "SELECT userPw FROM team4.user where Id = ?";
 		try (Connection conn = ConnectionProvider.makeConnection();
 				PreparedStatement stmt = conn.prepareStatement(sql)){
-			stmt.setString(1, userId);
+			stmt.setInt(1, id);
 			try (ResultSet rs = stmt.executeQuery()) {
 				if(rs.next()) {
 					return rs.getString("userPw"); // 아디 있으면 비번 반환
@@ -24,7 +26,6 @@ public class Login {
 		}		
 		return null; // 아디 없으면 null 반환
 	}
-	
 	public static boolean checkUserPw(String userPw, String inputPw) {
 		if (userPw.equals(inputPw)) {
 			return true; // 비번과 입력 비번이 같으면 true 반환
@@ -32,20 +33,16 @@ public class Login {
 			return false; // 다르면 false 반환
 		}
 	}
-	public int idValue(String userId) {
-		String sql = "SELECT id FROM team4.user where userId = ?";
-		try (Connection conn = ConnectionProvider.makeConnection();
-				PreparedStatement stmt = conn.prepareStatement(sql)){
-			stmt.setString(1, userId);
-			try (ResultSet rs = stmt.executeQuery()){
-				if (rs.next()) {
-					return rs.getInt("id");
-				}
+	public static void accountDelete (boolean delete, int id) { // delete true면 회원탈퇴 아니면 안 함
+		if (delete) {
+			String sql = "DELETE FROM user WHERE id = ?";
+			try (Connection conn = ConnectionProvider.makeConnection();
+					PreparedStatement stmt = conn.prepareStatement(sql)) {
+				stmt.setInt(1, id);
+				stmt.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch(SQLException e) {
-			e.printStackTrace();
 		}
-		return 0;
 	}
-	
 }
